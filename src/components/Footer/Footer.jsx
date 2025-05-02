3b48ddf99e23f7f2c0cf9861027f1b813b524cab
unknown <juliofidanza@gmail.com>
2025-05-02 16:29:06 -0300

    Agregando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,42 @@
-import './Footer.css'
+import './Footer.css';
 
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
 
-function Footer(){
-    return (
-      
-      <div className='footerazo'>
-        <p>soy un footer</p>
+        <div className="footer-info">
+          <h3>Julio Fidanza</h3>
+          <p>Desarrollador Frontend Freelance especializado en React.</p>
+        </div>
+
+        <div className="footer-stack">
+          <h4>Stack Tecnológico</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </div>
+
+        <div className="footer-tools">
+          <h4>Herramientas</h4>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Figma</li>
+            <li>Firebase</li>
+          </ul>
+        </div>
+
       </div>
-    )
-  
-  
-  }
-export default Footer
+
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Julio Fidanza. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
